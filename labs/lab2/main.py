89faa7eec21053e66e88651cf7b2d47c1b2ef7c5
fankab <fnkb108@gmail.com>
2026-07-16 20:34:18 +0300

    fix

--- labs/lab2/main.py
+++ labs/lab2/main.py
@@ -1,20 +1,20 @@
 import argparse
 
 from experiments.task0 import task0
-# from experiments.task1_pml import task1
-# from experiments.task2_interface import task2
-# from experiments.task3_slab import task3
-# from experiments.task4_photonic_crystal import task4
-# from experiments.task5_cavity import task5
+from experiments.task1_pml import task1
+from experiments.task2_interface import task2
+from experiments.task3_slab import task3
+from experiments.task4_photonic_crystal import task4
+from experiments.task5_cavity import task5
 
 
 TASKS = {
     "task0": task0,
-    # "task1": task1,
-    # "task2": task2,
-    # "task3": task3,
-    # "task4": task4,
-    # "task5": task5,
+    "task1": task1,
+    "task2": task2,
+    "task3": task3,
+    "task4": task4,
+    "task5": task5,
 }
 
 
